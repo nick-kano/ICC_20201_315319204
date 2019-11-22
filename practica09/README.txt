8dd903c9fc9dc270cb0a88ffcb81ffa148ca819f
Nicolas Kano <nick.kano@ciencias.unam.mx>
2019-11-21 20:21:02 -0600

    corrijo la practica 9

---
+++ practica09/README.txt
@@ -0,0 +1,6 @@
+Para compilar el archivo siga los siguientes pasos
+1) abra la terminal
+2) con el comando cd cambie de carpeta al archivo donde tenga esta carpeta, por ejemplo si esta en descargas el comando se veria cd /Downloads/practica09
+3) compile el archivo usando el comando javac WriteAndRead.javac
+4) el programa leera los puntos del archivo coordenadas.txt y los convertira a cordenadas polares, si quiere agregar mas puntos o quitar algunos puede modificar el archivo coordenadas.txt
+5) una vez que quiera convertir los puntos utilice el comando java WriteAndRead y este creara un archivo txt llamado polares donde estara las conversiones a coordenadas polares del archivo cordenadas.txt
--- practica09/WriteAndRead.java
+++ practica09/WriteAndRead.java
@@ -3,29 +3,30 @@ import java.nio.file.Files;
 import java.util.List;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.lang.Math;
 public class WriteAndRead{
 	public static void main(String... args){
+		String aux="",aux4="";
+		double aux2=0.0,aux3=0.0;
 		//escritor de archivos
 		try{
 			//archivo que se va a crear/escribir
-			Path path=Paths.get("beep.txt");
-			//lo que se va a escribir
-			Files.write(path,"line 1\nline 2\n".getBytes());
-		}catch(IOException e){
-			System.err.println(e.getMessage());
-		}
-		//lector de archivos
-		try{
+			Path path2=Paths.get("polares.txt");
 			//archivo que se va a leer
 			Path path=Paths.get("coordenadas.txt");
-			//se leen todas las lineas del archivo
+			//se leen las lineas del archivo
 			List<String> lines=Files.readAllLines(path);
-			//por cada linea
+			//por cada linea convertimos las cordenadas que vienen a polares
 			for(String string:lines){
 				String row[]=string.split(",");//separamos en un array donde cada cosa entre comas es un elemento
-				System.out.println("cord x: "+row[0]+"cord y: "+row[1]);// imprimimos en consola lo que leimos
+				aux2=Math.sqrt((Double.parseDouble(row[0])*Double.parseDouble(row[0]))+(Double.parseDouble(row[1])*Double.parseDouble(row[1])));//magnitud del vector
+				aux3=Math.atan(Double.parseDouble(row[1])/Double.parseDouble(row[0]));//angulo del vector
+				aux="("+aux2+","+aux3+")\n";
+				aux4+=aux;//buffer de la linea
 			}
-		}catch(IOException e){
+			Files.write(path2,aux4.getBytes());//la escritura del archivo
+		}
+		catch(IOException e){
 			System.err.println(e.getMessage());
 		}
 	}
